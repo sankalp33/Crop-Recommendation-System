01ac6230d7058751df0dc6e185426c82dbc3b677
sankalp33 <sankalpparab333@gmail.com>
2021-05-24 16:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,url_for,redirect,render_template,jsonify
+from flask import Flask,request,url_for,redirect,render_template
 import pandas as pd
 import pickle
 import numpy as np
